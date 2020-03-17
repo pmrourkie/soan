eb8c425a5842c5605a85f6e993d2e64ec2f74a08
Rich Allen <rourkie@gmail.com>
2020-03-17 17:16:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='maartengrootendorst@gmail.com',
     packages=['soan'],
     description='Used to analyze whatsapp data',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=['numpy', 'PIL', 'scipy', 'sklearn', 'regex', 're', 'emoji', 'seaborn',
                      'datetime', 'itertools', 'pandas', 'operator', 'requests', 'mpl_toolkits', 
                       'wordcloud', 'pattern', 'palettable'],
